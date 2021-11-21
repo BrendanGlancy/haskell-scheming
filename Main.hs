eef9c51e8cb4ab3f2e4003b0310b7c5d37fc1c5e
BrendanGlancy <bglance68@gmail.com>
2021-11-21 01:50:30 -0500

    parsing stuff

--- Main.hs
+++ Main.hs
@@ -1,10 +1,40 @@
 module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
 
+-- Return values
 data LispVal = Atom String
              | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
 
+-- Parses string outputs LispVal
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+             first <- letter <|> symbol
+             rest <- many (letter <|> digit <|> symbol)
+             let atom = first:rest
+             return $ case atom of 
+                        "#t" -> Bool True
+                        "#f" -> Bool False
+                        _    -> Atom atom
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -14,12 +44,13 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 -- paser function 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+    Right _ -> "Found value"
 
 -- main function 
 main :: IO ()
 main = do 
          (expr:_) <- getArgs
          putStrLn (readExpr expr)
+
