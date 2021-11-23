52119181b9297cbb432bfefd915e5c4a92a20ef0
BrendanGlancy <bglance68@gmail.com>
2021-11-23 13:58:33 -0500

    functionality of very basic calculator 🧮

--- Main.hs
+++ Main.hs
@@ -92,6 +92,38 @@ eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+-- recursively evaluate each argument, map eval over the args
+-- lets up compound expressions like (+ 2 (- 3 1) (* 5 4))
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+-- supported primitives
+-- type primative is a list of pairs
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+-- takes a primative Haskell function (often an operator section) 
+-- and wraps it with code to unpack an argument list, 
+-- wrap the result up in our Number constructor
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in
+                           if null parsed
+                              then 0
+                              else fst $ parsed !! 0
+unpackNum (List [n])= unpackNum n
+unpackNum _ = 0
 
 -- paser function 
 readExpr :: String -> LispVal
